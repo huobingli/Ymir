cba3c235ec8b6f713473e70ea9c0e1eb606b8557
huobingli <huobingli@outlook.com>
2021-04-17 23:57:46 +0800

    update

---
+++ config/projectconfig.py
@@ -0,0 +1,14 @@
+# 工具路径
+
+# curl 
+curl_tool = "/tools/curl/curl.exe"
+
+# git
+git_tool = "/tools/git/git.exe"
+
+# svn
+svn_tool = "/tools/svn/svn.exe"
+
+# url
+upload_url = ""
+download_url = ""
--- operate/gitoperate.py
+++ operate/gitoperate.py
@@ -1,4 +1,5 @@
 from comm import comm
+from os 
 
 # 是否是git目录，判断是否包含.git文件夹
 def is_git_dir(_work_dir):
@@ -9,3 +10,26 @@ def is_git_dir(_work_dir):
     if comm.is_exists(check_path):
         return True
     return False
+
+# 获取git工具
+def _get_git_tools():
+    return '"' + os.path.dirname(os.path.abspath(__file__)) + '/../tools/git/bin/git.exe"'
+
+# 拉去分支
+def git_pull(remote_path, loacl_path):
+    git_exe = _get_git_tools()
+    
+    git_cmd = git_exe
+    return comm.sync_exec(git_cmd)
+
+# 克隆分支
+def git_clone(remote_path, loacl_path):
+    pass
+
+# 切换分支
+def git_switch_branch(git_branch):
+    pass
+
+# 切换tag
+def git_switch_tag(git_tag):
+    pass
